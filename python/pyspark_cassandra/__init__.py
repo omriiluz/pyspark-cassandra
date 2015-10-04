2058c94eca6f7c23a2bfde7914225ec738e7e043
omriiluz <omri@iluz.net>
2015-10-04 02:26:59 -0700

    Revert "initial support for joinWithCassandraTable"
    This reverts commit 32b3c9978f35f750eaa6a5cea2f9e3de9b20d199.

--- python/pyspark_cassandra/__init__.py
+++ python/pyspark_cassandra/__init__.py
@@ -1,9 +1,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ import pyspark_cassandra.rdd
 
 from .conf import WriteConf
 from .context import CassandraSparkContext, monkey_patch_sc
-from .rdd import CassandraRDD, saveToCassandra, RowFormat, joinWithCassandraTable
+from .rdd import CassandraRDD, saveToCassandra, RowFormat
 from .types import Row, UDT
 
 
@@ -42,7 +42,6 @@ __all__ = [
 
 # Monkey patch the default python RDD so that it can be stored to Cassandra as CQL rows
 pyspark.rdd.RDD.saveToCassandra = saveToCassandra
-pyspark.rdd.RDD.joinWithCassandraTable = joinWithCassandraTable
 
 # Monkey patch the sc variable in the caller if any
 parent_frame = inspect.currentframe().f_back
